412b9d15d20311d6b0bd37f563bbec028a4642de
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-01-17 14:54:51 -0600

    Bugtesting

--- payCalculator.py
+++ payCalculator.py
@@ -11,4 +11,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+## calculatePay()
